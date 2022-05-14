9a6aba027878f7810b29b0e00d8c64fb4592cc24
cleber <cleber.r.sudul@gmail.com>
2022-05-14 16:05:25 -0300

    Ajustando o Controller

--- RestCore5/RestCore5/Startup.cs
+++ RestCore5/RestCore5/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestCore5.Services;
+using RestCore5.Services.Implementations;
 
 namespace RestCore5
 {
@@ -32,6 +27,9 @@ namespace RestCore5
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestCore5", Version = "v1" });
             });
+
+            // Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
